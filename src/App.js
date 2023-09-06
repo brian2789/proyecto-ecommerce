0a0cbd46cd61b568cf9c0130294f3510acdb2142
Brian Arce <brianarce2789@gmail.com>
2023-09-06 13:09:36 -0300

    primer-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Productos from './componentes/Productos';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React-live</h1>
+      <Productos/>
+    </div>
+  );
+}
+
+export default App;
